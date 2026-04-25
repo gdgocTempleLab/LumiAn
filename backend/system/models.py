41ea7d2b44e574ffb40466873cd8ea39e488244a
Shawn Lin <spig100.roc@gmail.com>
2026-04-25 13:58:13 +0800

    feat: Finish user creating API
    issue: #6

--- backend/system/models.py
+++ backend/system/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
 
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> e050925 (feat: Finish user creating API)
 class RoleChoices(models.TextChoices):
     ADMIN = 'Admin', '寺廟管理員'
     VOLUNTEER = 'Volunteer', '志工'
@@ -11,8 +14,11 @@ class AccountStatusChoices(models.TextChoices):
     INACTIVE = 'Inactive', '停用'
     SUSPENDED = 'Suspended', '停權'
 
+<<<<<<< HEAD
 =======
 >>>>>>> e38dc17 (feat: Modify user model and default admin creation logic)
+=======
+>>>>>>> e050925 (feat: Finish user creating API)
 class UserAccountManager(BaseUserManager):
     def create_user(self, account, email, password=None, **extra_fields):
         if not account:
@@ -25,16 +31,22 @@ class UserAccountManager(BaseUserManager):
 
     def create_superuser(self, account, email, password=None, **extra_fields):
 <<<<<<< HEAD
+<<<<<<< HEAD
         extra_fields.setdefault('role', RoleChoices.ADMIN)
         extra_fields.setdefault('Account_Status', AccountStatusChoices.ACTIVE)
 =======
         extra_fields.setdefault('role', 'Admin')
         extra_fields.setdefault('Account_Status', 'Active')
 >>>>>>> e38dc17 (feat: Modify user model and default admin creation logic)
+=======
+        extra_fields.setdefault('role', RoleChoices.ADMIN)
+        extra_fields.setdefault('Account_Status', AccountStatusChoices.ACTIVE)
+>>>>>>> e050925 (feat: Finish user creating API)
         return self.create_user(account, email, password, **extra_fields)
 
 class User_Account(AbstractBaseUser):
     user_id = models.AutoField(primary_key=True)
+<<<<<<< HEAD
 <<<<<<< HEAD
     role = models.CharField(max_length=50, choices=RoleChoices.choices, default=RoleChoices.VOLUNTEER) # 角色(寺廟管理員/志工)
     email = models.CharField(max_length=255)
@@ -48,6 +60,13 @@ class User_Account(AbstractBaseUser):
     password = models.CharField(max_length=128, db_column='password_hash', unique=True)
     Account_Status = models.CharField(max_length=50)
 >>>>>>> e38dc17 (feat: Modify user model and default admin creation logic)
+=======
+    role = models.CharField(max_length=50, choices=RoleChoices.choices, default=RoleChoices.VOLUNTEER) # 角色(寺廟管理員/志工)
+    email = models.CharField(max_length=255)
+    account = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=128, db_column='password_hash', unique=True)
+    Account_Status = models.CharField(max_length=50, choices=AccountStatusChoices.choices, default=AccountStatusChoices.ACTIVE)
+>>>>>>> e050925 (feat: Finish user creating API)
     Account_Creation_Time = models.DateTimeField(auto_now_add=True)
     Account_Update_Time = models.DateTimeField(auto_now=True)
 
--- backend/system/urls.py
+++ backend/system/urls.py
@@ -1,13 +1,18 @@
 from django.urls import path
 <<<<<<< HEAD
+<<<<<<< HEAD
 from .views import LoginView, TemplateAPIView, AccountListView, CreateAccountView, AccountDetailView, UpdateAccountView, UpdateProfileView, ChangePasswordView, AdminResetPasswordView
 =======
 from .views import LoginView, TemplateAPIView, AccountListView
 >>>>>>> 09f9d84 (feat: implement authentication login and account listing API endpoints)
+=======
+from .views import LoginView, TemplateAPIView, AccountListView, CreateAccountView
+>>>>>>> e050925 (feat: Finish user creating API)
 
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('account/list/', AccountListView.as_view(), name='account_list'),
+<<<<<<< HEAD
 <<<<<<< HEAD
     path('account/create/', CreateAccountView.as_view(), name='create_account'),
     path('account/detail/', AccountDetailView.as_view(), name='account_detail'),
@@ -17,6 +22,9 @@ urlpatterns = [
     path('profile/change-password/', ChangePasswordView.as_view(), name='change_password'),
 =======
 >>>>>>> 09f9d84 (feat: implement authentication login and account listing API endpoints)
+=======
+    path('account/create/', CreateAccountView.as_view(), name='create_account'),
+>>>>>>> e050925 (feat: Finish user creating API)
     
     # ==========================================
     # API Route Template
